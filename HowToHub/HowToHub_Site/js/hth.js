873dba4bcf3d026ee6f0c4ae9a518fb475aa7bfa
Dean Stallworth <deans3@fullsail.edu>
2013-03-22 04:19:07 -0500

    Tidy Up/Learn More/Login and JSON
    Made the site a little more aesthetically pleasing.  Added the learn
    more page.
    
    Tried to correct the login oauth 2 and parse json from reddits feed.
    Hit roadblocks on both.

--- HowToHub/HowToHub_Site/js/hth.js
+++ HowToHub/HowToHub_Site/js/hth.js
@@ -1,24 +1,31 @@
 $(function(){
-
-var form = $('form button');
-
-form.ajax({
-	type: "POST",
-	url: "[https://accounts.google.com/o/oauth2/token]", // your POST target goes here
-	dataType: "json",
-	data: { [POST /o/oauth2/token HTTP/1.1
-		Host: accounts.google.com
-		Content-Type: application/x-www-form-urlencoded
-		code=4/eYE0fVwwzwJo2twBg4kvFwkzTf6o.0q2cekhix3MXshQV0ieZDAo8j1T_egI&
-		client_id=1048828436085.apps.googleusercontent.com&
-		client_secret=L9ptxKkcDF72exFZTxYal_RR&
-		redirect_uri=localhost/oauth2callback&
-		grant_type=authorization_code
-		] }, // message to send goes here
-
-		// success: function (data){
-		// 	var obj = $.parseJSON(data);
-		// }
-	});
-
+// 	$.ajax({
+//    cache:     'true',
+//    async:     'false',
+//    type:      'GET',
+//    url:       'http://www.reddit.com/r/diy.json', // Point to your url
+//    dataType:  'jsonp',
+//    timeout:   1750,   
+//    success: function (info) {
+//    		$('#vidList').empty();
+//    		$('#ititemplate').tmpl(info).appendTo('#vidList li');   
+//             }                                  //
+// }).done(function()$(this).something("done"));   
+	// $.ajax({
+	// 	type: "POST",
+	// 	url: "https://accounts.google.com/o/oauth2/token/code=4/ux5gNj-_mIu4DOD_gNZdjX9EtOFf&
+	// 	+client_id=1048828436085.apps.googleusercontent.com&client_secret=L9ptxKkcDF72exFZTxYal_RR&
+	// 	+redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code",
+	// 	Host: 'accounts.google.com',
+	// 	ContentType: 'application/x-www-form-urlencoded',
+	// });
+	$.getJSON('http://www.reddit.com/r/diy.json', { get_param: 'value' }, function(data) {
+    $.each(data, function(index, element) {
+        $('body').append($('<div>', {
+            text: element.name
+        }));
+    });
+});
+    // };
+// });
 });
