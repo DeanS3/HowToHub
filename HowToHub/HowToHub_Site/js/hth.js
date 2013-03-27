87b9c91e11c2a2986b90148ec741a7bc649af6ee
Dean Stallworth <deans3@fullsail.edu>
2013-03-27 04:04:31 -0500

    Comments
    Commented on code in all pages.  Deleted some unneccessary code blocks
    that were no longer being used. Cleaned up a few pages and made notes
    of future changes to the code.

--- HowToHub/HowToHub_Site/js/hth.js
+++ HowToHub/HowToHub_Site/js/hth.js
@@ -1,18 +1,16 @@
 $(function(){
 
+// This page is being used to get info from Youtube and parse the data into the sidebar
+// on the user.html page.
 
-// $('enterBtn').addClass('disabled');
+var vids = $('#vidList')//Variable set up 
 
-
-var vids = $('#vidList')
-
-
+// Pulls info from Youtube and puts it in data object
 $.get('https://gdata.youtube.com/feeds/api/videos?q=DIY&orderby=published&start-index=1&max-results=10&prettyprint=true&v=2', function(data) {
 $('.result').html(data);
 alert('Load was performed.');
-// console.log(data);
 
-
+// Parsing and displaying parsed information by appending it to the variable from above
 $(data).find("entry").each(function()
 {
   $(vids).append($(this).find("Title").text() + "<br />");
@@ -27,7 +25,7 @@ $(data).find("entry").each(function()
 });
 
 });
-
+// Getting more information(titles) to display along with the above iformation
 $.get('https://accounts.google.com/o/oauth2/approval?as=-33b6894774fe2296&hl=en&pageId=none&xsrfsign=APsBz4gAAAAAUU0Q5Rq7KdxHveVFErSLrdhAmVz2R6pl', function(data){
 $('results').html(data).find('title')
 console.log($('results').html(data).find('title')
@@ -37,38 +35,5 @@ console.log($('results').html(data).find('title')
 $.post('https://accounts.google.com/o/oauth2/token', function(data) {
 $('.result').html(data);
 });
-// var xml = "https://gdata.youtube.com/feeds/api/videos?q=DIY&orderby=published&start-index=1&max-results=10&v=2",
-//  var xmlDoc = $.parseXML(data),
-//  $xml = $( xmlDoc ),
-// $title = $xml.find( "link" );
-// console.log('link');
-// 	$.ajax({
-//    cache:     'true',
-//    async:     'false',
-//    type:      'GET',
-//    url:       'http://www.reddit.com/r/diy.json', // Point to your url
-//    dataType:  'jsonp',
-//    timeout:   1750,   
-//    success: function (info) {
-//    		$('#vidList').empty();
-//    		$('#ititemplate').tmpl(info).appendTo('#vidList li');   
-//             }                                  //
-// }).done(function()$(this).something("done"));   
-	// $.ajax({
-	// 	type: "POST",
-	// 	url: "https://accounts.google.com/o/oauth2/token/code=4/ux5gNj-_mIu4DOD_gNZdjX9EtOFf&
-	// 	client_id=1048828436085.apps.googleusercontent.com&client_secret=L9ptxKkcDF72exFZTxYal_RR&
-	// 	redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code",
-	// 	Host: 'accounts.google.com',
-	// 	ContentType: 'application/x-www-form-urlencoded',
-	// });
-// 	$.getJSON('http://www.reddit.com/r/diy.json', { get_param: 'value' }, function(data) {
-//     $.each(data, function(index, element) {
-//         $('body').append($('<div>', {
-//             text: element.name
-//         }));
-//     });
-// });
-    // };
-// });
+
 });
